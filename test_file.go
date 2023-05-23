0deed4e1fe6e75d10f11c3cf7d599e0f1fc5cbca
pritam dutta <rohitdutta907@gmail.com>
2023-05-24 03:32:20 +0530

    Testing Custome Var

--- test_file.go
+++ test_file.go
@@ -1,3 +1,16 @@
 package test
 
+import (
+	"fmt"
+)
+
 var Massage = "This is for only Testing Perpose"
+
+type Routes map[string]func() string
+
+func PrintRoutes(_routes Routes) {
+	for r, function := range _routes {
+		v := function()
+		fmt.Println(r, v)
+	}
+}
